6fbc3f0ded85d397253ecb19eeeb5f99a13da145
Leo <leo.dumont@epitech.eu>
2024-02-06 18:49:25 +0100

    feat(MLC-6): added mmeove implementation in asm

---
+++ src/memmove.asm
@@ -0,0 +1,33 @@
+section .text
+global memmove
+
+; memmove: copies n bytes from memory area src to memory area dest
+; Input: rdi - the pointer to the destination memory block
+;        rsi - the pointer to the source memory block
+;        rdx - the number of bytes to copy
+; Output: rax - the pointer to the destination memory block
+memmove:
+    mov rax, rdi
+    xor rcx, rcx
+    cmp rdi, rsi
+    jge .memmove_loop_inverse
+    jmp .memmove_loop
+
+.memmove_loop:
+    cmp rcx, rdx
+    je end
+    mov al, byte [rsi + rcx]
+    mov byte [rdi + rcx], al
+    inc rcx
+    jmp .memmove_loop
+
+.memmove_loop_inverse:
+    cmp rdx, -1
+    je end
+    mov al, byte [rsi + rdx - 1]
+    mov byte [rdi + rdx - 1], al
+    dec rdx
+    jmp .memmove_loop_inverse
+
+end:
+    ret
